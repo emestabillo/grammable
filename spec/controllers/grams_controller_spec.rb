64db30f29dc5c2f605f4f30f8034a9e459b29183
Emmilie Estabillo <emsestabillo@gmail.com>
2017-04-09 16:03:38 +0000

    Edit and update gram2

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#edit action" do
     it "should successfully show the edit form if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 error message if the gram is not found" do
+      get :edit, params: { id: 'SWAG' }
+      expect(response).to have_http_status(:not_found)
     end
   end
 
@@ -20,9 +25,9 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#show action" do
-    it "should successfully show the page of the gram is found" do
+    it "should successfully show the page if the gram is found" do
       gram = FactoryGirl.create(:gram)
-      get :show, params: { id: gram.id}
+      get :show, params: { id: gram.id }
       expect(response).to have_http_status(:success)
     end
 
